17d7cd867bfc4af73f23132e883f25f02268f4ae
Anna <aanikonova15@gmail.com>
2022-02-12 23:18:52 +0300

    Add Task 24

---
+++ Task_24/Program.cs
@@ -0,0 +1,19 @@
+﻿// Найти кубы чисел от 1 до N
+
+int n = 10;
+int x = 1;
+int i=1;
+
+while(x<=n)
+{
+    Console.Write(x + "\t");
+    x++;
+}
+Console.WriteLine();
+
+while(i<=n)
+{
+    int i1 = i*i*i;
+    Console.Write(i1 + "\t");
+    i++;
+}
