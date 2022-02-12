9199668f1f50bdf9bb16d4186cdfde87d31bfd1e
Anna <aanikonova15@gmail.com>
2022-02-12 18:25:58 +0300

    Add Task 18

---
+++ Task_18/Program.cs
@@ -0,0 +1,22 @@
+﻿// Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+
+
+Console.WriteLine("Введите значение x и y (true/false)");
+
+Console.Write("x = ");
+bool x = bool.Parse(Console.ReadLine());
+
+Console.Write("y = ");
+bool y = bool.Parse(Console.ReadLine());
+
+ if (!(x || y) == !x && !y)
+    {
+        Console.WriteLine("Истина");
+    }
+
+else
+    {
+        Console.WriteLine("Не истина");
+    }
+
+
